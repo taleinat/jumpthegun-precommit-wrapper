c6be907be901108d2ed0e400416ec0b54c36b6e9
Tal Einat <532281+taleinat@users.noreply.github.com>
2025-05-27 22:36:25 +0300

    Fix entrypoint.sh to work when installed via pipx

--- jumpthegun_precommit_wrapper/entrypoint.sh
+++ jumpthegun_precommit_wrapper/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-script_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+script_path="$0"
+# If this is a symlink, read it.
+if [ -L "$script_path" ]; then
+  script_path="$(readlink "$script_path")"
+fi
+script_dir="$(CDPATH= cd -- "$(dirname -- "$script_path")" && pwd)"
 export PATH="$script_dir":"$PATH"
 exec jumpthegun run jumpthegun-precommit-wrapper-main "$@"
